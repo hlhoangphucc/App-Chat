34dbfe92a9f47f599dd0676d41153c934d73c66d
hoangphuc <hlhoangphucc@gmail.com>
2023-09-06 23:42:37 +0700

    WelCome

--- AppChat/App.js
+++ AppChat/App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import styles from './App.component';
+import SearchScreen from './components/search';
+import WelcomeScreen from './components/welcome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WelcomeScreen />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
